a94d95b3bfabc270486a664be5952e5961021063
Christopher Szczyglowski <cs1414@bristol.ac.uk>
2020-03-08 16:45:01 +0000

    New function for generating code coverage

---
+++ dev_tools/gen_code_coverage.m
@@ -0,0 +1,47 @@
+function [TestResult, fList, pList] = gen_code_coverage
+%gen_code_coverage Generates a code coverage report and obtains the
+%product dependencies for the package. 
+%
+% Syntax:
+%	- Generate the code coverage report and return the product list...
+%       >> [] = gen_code_coverage;
+%
+% Detailed Description:
+%	-  Generates a code coverage report and obtains the product
+%	   dependencies for the package by running the test framework in
+%	   'TestMatran.m'.
+%
+% See also: matlab.unittest.plugins.CodeCoveragePlugin
+%           matlab.codetools.requiredFilesAndProducts
+%
+% References:
+%	[1]. 
+%
+% Author    : Christopher Szczyglowski
+% Email     : chris.szczyglowski@gmail.com
+% Timestamp : 08-Mar-2020 15:18:58
+%
+% Copyright (c) 2020 Christopher Szczyglowski
+% All Rights Reserved
+%
+%
+% Revision: 1.0 08-Mar-2020 15:18:58
+%	- Initial function:
+%
+% <end_of_pre_formatted_H1>
+
+import matlab.unittest.TestSuite
+import matlab.unittest.TestRunner
+import matlab.unittest.plugins.CodeCoveragePlugin
+
+%Generate the code coverage report
+suite = TestSuite.fromFile('TestMatran.m');
+
+runner = TestRunner.withTextOutput;
+runner.addPlugin(CodeCoveragePlugin.forFolder(pwd));
+TestResult = runner.run(suite);
+
+%Get the package dependencies
+[fList, pList] = matlab.codetools.requiredFilesAndProducts('TestMatran.m');
+
+end
